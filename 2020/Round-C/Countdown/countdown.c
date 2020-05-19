4727a78582740b47245bccdcbef7868a3e3fb553
Befeepilf <befeepilf@protonmail.com>
2020-05-19 15:32:15 +0200

    2020 C Countdown: use fscanf() instead of read()

--- 2020/Round-C/Countdown/countdown.c
+++ 2020/Round-C/Countdown/countdown.c
@@ -4,22 +4,23 @@
 #include <stdbool.h>
 #include <math.h>
 
-unsigned long getNextLong(void);
-
 int main(void)
 {
-    unsigned long num_tests = getNextLong();
+    unsigned int num_tests;
+    fscanf(stdin, "%u", &num_tests);
 
     for (unsigned long i = 0; i < num_tests; i++)
     {
-        unsigned long len = getNextLong();
-        unsigned long k = getNextLong();
+        unsigned int len, k;
+        fscanf(stdin, "%u %u", &len, &k);
 
         unsigned long countdown = 0;
         unsigned long num_countdowns = 0;
         for (unsigned long j = 0; j < len; j++)
         {
-          unsigned long a = getNextLong();
+          unsigned int a;
+          fscanf(stdin, "%u", &a);
+
           // if start of k-countdown has been found
           if (countdown > 0)
           {
@@ -52,35 +53,4 @@ int main(void)
 
         printf("Case #%lu: %lu\n", i + 1, num_countdowns);
     }
-}
-
-// reads and returns the next longeger found in stdin
-unsigned long getNextLong(void)
-{
-  char digits[32];
-  unsigned long i = 0;
-  bool stop = false;
-
-  while (!stop)
-  {
-    read(0, &digits[i], sizeof(char));
-    if (isdigit(digits[i]))
-    {
-      i++;
-    }
-    else if (i > 0)
-    {
-      stop = true;
-    }
-  }
-
-  unsigned long res = 0;
-  for (unsigned long j = i; i > 0; i--)
-  {
-    // get the number represented by the ASCII character
-    // 0 has an ASCII code of 48
-    res += pow(10, (i - 1)) * (digits[j - i] - 48);
-  }
-
-  return res;
 }
