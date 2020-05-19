58dea33259263a3ff5a4c341377b50f537a6b8b7
Befeepilf <befeepilf@protonmail.com>
2020-05-19 16:13:12 +0200

    2020 C Countdown: decluttering

--- 2020/Round-C/Countdown/countdown.c
+++ 2020/Round-C/Countdown/countdown.c
@@ -1,56 +1,46 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include <math.h>
 
 int main(void)
 {
     unsigned int num_tests;
     fscanf(stdin, "%u", &num_tests);
 
-    for (unsigned long i = 0; i < num_tests; i++)
+    for (unsigned int i = 0; i < num_tests; i++)
     {
         unsigned int len, k;
         fscanf(stdin, "%u %u", &len, &k);
 
-        unsigned long countdown = 0;
-        unsigned long num_countdowns = 0;
-        for (unsigned long j = 0; j < len; j++)
+        unsigned int countdown = 0;
+        unsigned int num_countdowns = 0;
+        for (unsigned int j = 0; j < len; j++)
         {
           unsigned int a;
           fscanf(stdin, "%u", &a);
 
-          // if start of k-countdown has been found
-          if (countdown > 0)
+          // if beginning of k-countdown
+          if (a == k) countdown = a;
+          /// if start of k-countdown has already been found
+          else if (countdown > 0)
           {
             // if a is next in countdown
             if (a == countdown - 1)
             {
-              // if a is end of countdown
-              if (a == 1)
+              // if a is not end of countdown
+              if (a != 1) countdown = a;
+              else
               {
                 num_countdowns++;
                 countdown = 0;
-              }
-              else
-              {
-                countdown = a;
               }
             }
             // reset countdown
             else
             {
-              countdown = a == k ? a : 0;
+              countdown = 0;
             }
-          }
-          // beginning of k-countdown
-          else if (a == k)
-          {
-            countdown = a;
           }
         }
 
-        printf("Case #%lu: %lu\n", i + 1, num_countdowns);
+        printf("Case #%i: %i\n", i + 1, num_countdowns);
     }
 }
